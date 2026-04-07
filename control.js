599b3f7676c632c6ec3b3be0bea3b22c2417522b
hall0www <lukefunnyspam@gmail.com>
2026-04-07 12:49:38 -0700

    Update control.js

--- control.js
+++ control.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const text1 = document.getElementById("text1");
   const text1 = document.getElementById("text2");
   if (text1) {
-    text1.style.transform = "translateX(600px)";
+    text1.style.transform = "translateX(1600px)";
   }
    if (text2) {
-    text2.style.transform = "translateX(600px)";
+    text2.style.transform = "translateX(1600px)";
      text2.style.transform = "translateY(px)50";
   }
 });
