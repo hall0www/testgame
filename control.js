6131291219065038fb121c60153b8e7acf0a1da5
hall0www <lukefunnyspam@gmail.com>
2026-04-07 12:23:25 -0700

    Update control.js

--- control.js
+++ control.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const text1 = document.getElementById("text1");
   if (text1) {
-    text1.style.transform = "translateX(50px)";
+    text1.style.transform = "translateX(200px)";
     text1.style.opacity = "0.5";
   }
 });
